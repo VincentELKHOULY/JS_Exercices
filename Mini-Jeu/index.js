994f70317874cb3cb9e23520553246afc8837182
Vincent EL KHOULY <vinidev@outlook.fr>
2023-09-20 10:47:13 +0200

    Début du mini jeux, mise en place du systeme de clique sur le boutton rouge.

---
+++ Mini-Jeu/index.js
@@ -0,0 +1,10 @@
+//je selectionne et je stocke
+const btnRed = document.getElementById("btn-red");
+
+//Je soumet le cercle rouge  une action.
+
+btnRed.addEventListener("click", () => {
+  console.log("cliqué");
+  //ici l'action :
+  //incrémenter le compteur de 1
+});
