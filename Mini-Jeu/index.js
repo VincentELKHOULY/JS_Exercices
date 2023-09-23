259d66540893725523eba179f91dd286754eae98
Vincent EL KHOULY <vinidev@outlook.fr>
2023-09-23 14:42:40 +0200

    ajout du cercle bleu plus animation.

--- Mini-Jeu/index.js
+++ Mini-Jeu/index.js
@@ -1,10 +1,18 @@
 //je selectionne et je stocke
 const btnRed = document.getElementById("btn-red");
+const btnBlue = document.getElementById("btn-blue");
+let compteur = 0;
 
 //Je soumet le cercle rouge  une action.
 
 btnRed.addEventListener("click", () => {
-  console.log("cliqué");
   //ici l'action :
   //incrémenter le compteur de 1
+  compteur = compteur + 1;
+  console.log(compteur, "compteur aprés incrémentation");
+});
+
+btnBlue.addEventListener("click", () => {
+  compteur = compteur + 1;
+  console.log(compteur, "point cercle bleu");
 });
