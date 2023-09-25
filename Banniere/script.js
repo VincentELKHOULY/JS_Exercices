a5d0b36920b645952188f6e18c91bcb8c899d9c6
Vincent EL KHOULY <vinidev@outlook.fr>
2023-09-25 16:08:21 +0200

    Ajout de l'éevenement au clic sur le boutton + transition

---
+++ Banniere/script.js
@@ -0,0 +1,6 @@
+const btnSucess = document.querySelector(".btn-success");
+const coockies = document.querySelector(".coockies");
+
+btnSucess.addEventListener("click", () => {
+  coockies.style.opacity = "0";
+});
